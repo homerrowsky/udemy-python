6967e7dc91c03d2f017211236cbbf673bbec7140
homerrowsky <homerrowsky@yahoo.com.mx>
2017-07-07 09:34:09 -0500

    Evalua lo tecleado filtrando expresión mediante regex **BASICO**

--- calculadora.py
+++ calculadora.py
@@ -3,20 +3,24 @@ import re
 print('Calculadora en python')
 print('Teclea "salir" para terminar')
 
-previous = 0
+previa = 0
 run = True
 
 def realizaCalculo():
     
     # Accesar a variable run global para que pueda parar ejecucion al teclear 'salir'
+    # y a previa para mantener operacion aritmetica actual
     global run
+    global previa
 
     ecuacion = input('Teclea la operacion: ')
     if ecuacion == "salir":
         print('\n\nCerrando...... Hasta pronto!!')
         run = False
     else:
-        print('Tecleaste la operacion: ', ecuacion)
+        ecuacion = re.sub('[a-zA-Z,:.\'()" "]', '', ecuacion)
+        previa = eval(ecuacion)
+        print('Tecleaste la operacion: ', previa)
 
 # Empieza ejecución
 while run:
